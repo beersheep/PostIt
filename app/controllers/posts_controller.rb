48793d9e2f1753044495940ec09bcb5ff40d3c93
beersheep <beersheep0109@gmail.com>
2015-11-24 19:18:55 +0800

    set posts route and generate random database data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: :show
   def index
+    @post = Post.all
+  end
 
+  def show
+    @post
+  end
+
+
+  private 
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# require 'faker'
+# include Faker
+
+21.times do
+  users = User.create(
+    username: Faker::Internet.user_name
+    )
+end
+
+21.times do 
+  posts = Post.create(
+    user_id: rand(21),
+    title: Faker::Book.title,
+    url: Faker::Internet.url,
+    description: Faker::Hacker.say_something_smart
+    )
+end
+
+10.times do
+  categories = Category.create(
+    name: Faker::Book.genre)
+end
+
+posts = Post.all
+
+posts.each do |post|
+  category = Category.all.shuffle!.pop
+  post.categories << category
+end
+
+
+25.times do 
+  comments = Comment.create(
+    user_id: rand(21), 
+    post_id: rand(21), 
+    body: Faker::Hacker.say_something_smart
+    )
+end
