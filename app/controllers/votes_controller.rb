92112f6a8f6301e9c307caa0b144c51e7a768b49
beersheep <beersheep0109@gmail.com>
2015-12-01 17:06:14 +0800

    fix the redirect issue(after voting)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,15 +3,15 @@ class VotesController < ApplicationController
   def create
 
     if has_voted?
-      @vote.vote.to_s == params[:vote] ? self.destroy : self.update
+      @vote.vote.to_s == params[:vote] ? self.destroy : self.change_vote
     else
       @vote = Vote.create(vote: params[:vote], voteable_type: params[:type], voteable_id: params[:id], user: current_user)
       if @vote.valid?
-        flash['notice'] = "Vote completed!"
-        redirect_to root_path
+        flash['notice'] = "Vote completed!" 
+        redirect_to_comments_or_index
       else
         flash['error'] = "Vote failed! Please try again"
-        redirect_to root_path
+        redirect_to_comments_or_index
       end
     end
   end
@@ -19,19 +19,19 @@ class VotesController < ApplicationController
   def destroy 
     Vote.find_by(voteable_type: params[:type], voteable_id: params[:id]).destroy
     flash[:success] = "vote uncount"
-    redirect_to root_path
+    redirect_to_comments_or_index
   end
 
-  def update
+  def change_vote
     @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
     @vote.vote = !@vote.vote
 
     if @vote.update(vote: params[:vote])
       flash[:success] = "change your vote"
-      redirect_to root_path
+      redirect_to_comments_or_index
     else 
       flash['error'] = "Vote failed! Please try again"
-      redirect_to root_path
+      redirect_to_comments_or_index
     end
   end
 
@@ -39,7 +39,15 @@ class VotesController < ApplicationController
   private
   def has_voted?
     @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
-    return @vote
+  end
+
+  def redirect_to_comments_or_index
+    if params[:type] == "Post"
+      redirect_to root_path
+    else 
+      @post = Comment.find(params[:id]).post
+      redirect_to post_path(@post)
+    end
   end
 
 end
