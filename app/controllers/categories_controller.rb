112ddff2ac26583208f84c0a98a3dcff750ef587
beersheep <beersheep0109@gmail.com>
2015-12-01 13:11:22 +0800

    subtle changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController 
-  before_action :require_user, only: [:create]
+  before_action :require_user, only: [:create, :new]
 
   def show
     @category = Category.find(params[:id])
