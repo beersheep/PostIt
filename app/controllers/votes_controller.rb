21734df6f27b8a54a11ba8ac9a16bcaecbbfcc0e
beersheep <beersheep0109@gmail.com>
2015-12-01 20:32:02 +0800

    Change button color if voted

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,13 @@
 class VotesController < ApplicationController
+  before_action :find_vote, only: [:change_vote, :destroy, :has_voted?]
 
   def create
 
     if has_voted?
+
       @vote.vote.to_s == params[:vote] ? self.destroy : self.change_vote
     else
+
       @vote = Vote.create(vote: params[:vote], voteable_type: params[:type], voteable_id: params[:id], user: current_user)
       if @vote.valid?
         flash['notice'] = "Vote completed!" 
@@ -16,14 +19,7 @@ class VotesController < ApplicationController
     end
   end
 
-  def destroy 
-    Vote.find_by(voteable_type: params[:type], voteable_id: params[:id]).destroy
-    flash[:success] = "vote uncount"
-    redirect_to_comments_or_index
-  end
-
   def change_vote
-    @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
     @vote.vote = !@vote.vote
 
     if @vote.update(vote: params[:vote])
@@ -35,11 +31,17 @@ class VotesController < ApplicationController
     end
   end
 
+  def destroy 
+    @vote.destroy
+    flash[:success] = "vote uncount"
+    redirect_to_comments_or_index
+  end
+
+  def has_voted? 
+  @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
+  end
 
   private
-  def has_voted?
-    @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
-  end
 
   def redirect_to_comments_or_index
     if params[:type] == "Post"
@@ -48,6 +50,11 @@ class VotesController < ApplicationController
       @post = Comment.find(params[:id]).post
       redirect_to post_path(@post)
     end
+  end
+
+
+  def find_vote
+    @vote = Vote.find_by(voteable_type: params[:type], voteable_id: params[:id], user: current_user)
   end
 
 end
