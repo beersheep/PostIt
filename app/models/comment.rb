b93587169fa9a1c9dc7a7d5e4d765b9d52ab4c74
beersheep <beersheep0109@gmail.com>
2015-12-10 16:53:13 +0800

    extract voteable to module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
+  include Voteable
+
   belongs_to :post
   belongs_to :creator, foreign_key:'user_id', class_name:'User'
-  has_many :votes, as: :voteable
 
   validates :body, presence: true
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,17 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :creator, foreign_key:'user_id', class_name:'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
+
   before_save :save_slug
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true 
   validates :categories, presence: true
+
 
   def generate_slug
     self.slug = self.title.strip.gsub(/\W/, "-").downcase
