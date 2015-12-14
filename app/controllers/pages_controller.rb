e1ef375ddc486120cc0fd5709c4cfd46c91b92ab
beersheep <beersheep0109@gmail.com>
2015-12-15 00:27:31 +0800

    finish pages [issue] refactor code and layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def show 
 
     all_posts = Post.all.includes(:comments, :creator, :categories, :votes)
-    @posts = all_posts.offset(params[:id].to_i * Post::PER_PAGE).limit(Post::PER_PAGE).order(id: :desc)
+    @posts = all_posts.offset((params[:id].to_i - 1) * Post::PER_PAGE).limit(Post::PER_PAGE).order(id: :desc)
+    total_pages = Post.all.size / Post::PER_PAGE.to_f
+    @pages = total_pages.ceil
   end
 end
